31c1dd11bc5d67a745ffed6c9f754882b8dc3920
RobertBroughton <103911380+RobertBroughton@users.noreply.github.com>
2023-04-21 18:45:02 -0400

    Update CarLotFX.java

--- src/carlot/CarLotFX.java
+++ src/carlot/CarLotFX.java
@@ -1,5 +1,4 @@
 package carlot;
-
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -12,21 +11,22 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+//Preprocesser
 
 public class CarLotFX extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
+//Launchs JavaFx
     final HBox hb = new HBox();
     CarLot lot = new CarLot();
-
+//Member Variables
     @Override
 	public void start(Stage stage) {
 		stage.setWidth(700);
-
+	//Creates Stage
 		TableView<Car> carView = new TableView<Car>();
-
+	//Creates TableView to hold cars data
 		final ObservableList<Car> data = FXCollections.observableArrayList();
 
 		TableColumn<Car, String> column1 = new TableColumn<>("ID");
@@ -52,12 +52,13 @@ public class CarLotFX extends Application {
 		TableColumn<Car, Double> column6 = new TableColumn<>("Sold");
 
 		column6.setCellValueFactory(new PropertyValueFactory<>("sold"));
-
+	//Creates columns for each data type
 		carView.getColumns().addAll(column1, column2, column3, column4, column5, column6);
+	//Adds the columns to the TableView carView
 		data.add(new Car("test1", 10000, 30, 12500.0D, 17500.0D));
 		data.add(new Car("test2", 12000, 20, 12000.0D, 15000.0D));
 		carView.setItems(data);
-
+	//Adds Cars to the table
 		final TextField addId = new TextField();
 		addId.setPromptText("ID");
 		addId.setMaxWidth(column1.getPrefWidth());
@@ -73,7 +74,7 @@ public class CarLotFX extends Application {
 		final TextField addPrice = new TextField();
 		addPrice.setMaxWidth(column5.getPrefWidth());
 		addPrice.setPromptText("Price");
-
+	//Creates TextFields for the different car data
 		final Button addButton = new Button("Add");
 		addButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -97,6 +98,7 @@ public class CarLotFX extends Application {
 				addPrice.clear();
 			}
 		});
+	//Creates Button to add a new car to the table from the TextFields and then clears the TextFields
 
 		final Button sellButton = new Button("Sell");
 		sellButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -123,7 +125,7 @@ public class CarLotFX extends Application {
 				addPrice.clear();
 			}
 		});
-
+	//Creates a Button that Updates the car at the indicated id to indicate that it is sold, the price sold at, and the profit from selling it, and then clears the TextFields
 		final Button profButton = new Button("Profit");
 		profButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -151,7 +153,7 @@ public class CarLotFX extends Application {
 				addPrice.clear();
 			}
 		});
-
+	//Creates a Button that creates an alert giving the profit of the car with the id in the Id TextField
 		carView.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
 			if (newVal != null) {
 				addId.setText(newVal.getId());
@@ -161,19 +163,20 @@ public class CarLotFX extends Application {
 				addPrice.setText(Double.toString(newVal.getSalesPrice()));
 			}
 		});
-
+	//Adds the Data of a selected car to the its Matching TextField
 		hb.getChildren().addAll(addId, addMileage, addMPG, addCost, addPrice, addButton, sellButton, profButton);
 		hb.setSpacing(3);
-
+	//Adds the buttons to the HBox
 		final VBox vbox = new VBox(carView);
-
+	//Creates a vBox and adds the TableView carView to it
 		vbox.getChildren().addAll(hb);
-
+	//Adds the HBox to the VBox
 		Scene scene = new Scene(vbox);
-
+	//Creates a Scene with the VBox
 		stage.setScene(scene);
-
+	//adds the scene to the stage
 		stage.show();
+	//Displays the Stage
 	}
 
 }
